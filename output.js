e9bc14d918970968edb4ecdf898186461daa6de9
Axi <98420477+axiand@users.noreply.github.com>
2024-10-05 03:41:59 +0200

    fix remaining bugs with model loading, begin work on upload

--- output.js
+++ output.js
@@ -1,11 +1,12 @@
 const { Model } = require('./lib/Model')
 
 function onLoad() {
+    console.log(JSON.stringify(model.lookupStrInt.entries()))
     for (i = 0; i < 500; i++) {
         console.log(model.getString())
-        console.log("\n")
+        console.log("--\n")
     }
 }
 
 let model = new Model()
-model.loadWeights("./data/model.dat", onLoad)
+model.loadWeights("model", onLoad)
